9e990201141f1e71e016cbbae3df5853fe899263
Mikko Sio <msio1@my.bcit.ca>
2024-02-04 19:07:22 -0800

    Added port constant

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const url = require("url");
 const utils = require("./modules/utils");
 const messages = require("./lang/messages/en/user");;
 
+const port = process.env.PORT || 8888;
+
 http.createServer(function (req, res) {
     const query = url.parse(req.url, true).query;
     let name = query.name || "Visitor";
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(messages.dateMessage.replace("%1", name) + utils.getDate());
-}).listen(8888);
-
-console.log("Server running at http://localhost:8888");
+}).listen(port);
