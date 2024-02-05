00ef365954ad90398c0637aa22c49ebe286b9941
Mikko Sio <msio1@my.bcit.ca>
2024-02-04 20:26:43 -0800

    Styled message to blue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer(function (req, res) {
     const query = url.parse(req.url, true).query;
     let name = query.name || "Visitor";
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(messages.dateMessage.replace("%1", name) + utils.getDate());
+    res.end(`<div style="color: blue;">${messages.dateMessage.replace("%1", name)} ${utils.getDate()}</div>`);
 }).listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
