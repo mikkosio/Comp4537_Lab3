fb2f367ad6f1a5d5904f7b4aec48156a84d328a5
Mikko Sio <msio1@my.bcit.ca>
2024-02-04 19:37:31 -0800

    Refactored response handling

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const url = require("url");
 const utils = require("./modules/utils");
 const messages = require("./lang/messages/en/user");
 
-const port = process.env.PORT || 8888;
-
-http.createServer(function (req, res) {
+module.exports = async (req, res) {
     const query = url.parse(req.url, true).query;
     let name = query.name || "Visitor";
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(messages.dateMessage.replace("%1", name) + utils.getDate());
-}).listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+};
