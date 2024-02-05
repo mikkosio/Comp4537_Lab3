66b41ff8a8e8a3729dbea967f90f7d7afdd53366
Mikko Sio <msio1@my.bcit.ca>
2024-02-04 20:08:18 -0800

    Fixed error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const url = require("url");
 const utils = require("./modules/utils");
 const messages = require("./lang/messages/en/user");
 
-module.exports = async (req, res) {
+module.exports = async (req, res) => {
     const query = url.parse(req.url, true).query;
     let name = query.name || "Visitor";
     res.writeHead(200, { "Content-Type": "text/html" });
