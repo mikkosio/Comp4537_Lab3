aec1320b947bda3166f3bb55932012f888484c95
Mikko Sio <msio1@my.bcit.ca>
2024-02-04 19:18:59 -0800

    Added log statement

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const url = require("url");
 const utils = require("./modules/utils");
-const messages = require("./lang/messages/en/user");;
+const messages = require("./lang/messages/en/user");
 
 const port = process.env.PORT || 8888;
 
@@ -10,4 +10,6 @@ http.createServer(function (req, res) {
     let name = query.name || "Visitor";
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(messages.dateMessage.replace("%1", name) + utils.getDate());
-}).listen(port);
+}).listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
